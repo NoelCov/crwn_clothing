4317fe7c56b4a7d475894ba5130107c3a235bc8b
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-07-07 01:21:26 -0700

    Removed internal react state and replaced it with redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,11 @@
-import React, { Component } from "react";
-import Shop_Data from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+import React from "react";
 
-export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component.jsx";
 
-    this.state = {
-      collections: Shop_Data,
-    };
-  }
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
-  render() {
-    const collections = this.state.collections;
-    return (
-      <div className="show-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+export default ShopPage;
